84e6ac7d9f592172332958dfbbfac6dc19f5f7a3
Oleg <iakovenkoua@gmail.com>
2017-01-15 20:14:05 +0200

    Add new class

---
+++ todomvctest/src/test/java/homework/third/v3/AtTodoMVCPageWithClearedDataAfterEachTest.java
@@ -0,0 +1,20 @@
+package homework.third.v3;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AtTodoMVCPageWithClearedDataAfterEachTest extends BaseTest {
+
+    @Before
+    public void openPage() {
+        open("https://todomvc4tasj.herokuapp.com/");
+    }
+
+    @After
+    public void clearData() {
+        executeJavaScript("localStorage.clear()");
+    }
+}
