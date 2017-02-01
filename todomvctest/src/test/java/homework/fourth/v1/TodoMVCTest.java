fbb4183ac6f302ab361be40802c9003c6eb2311a
Oleg <iakovenkoua@gmail.com>
2017-02-01 08:52:03 +0200

    code

--- todomvctest/src/test/java/homework/fourth/v1/TodoMVCTest.java
+++ todomvctest/src/test/java/homework/fourth/v1/TodoMVCTest.java
@@ -14,6 +14,7 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static homework.fourth.v1.TodoMVCTest.Task.*;
 
 
 public class TodoMVCTest extends AtTodoMVCPageWithClearedDataAfterEachTest {
@@ -21,7 +22,7 @@ public class TodoMVCTest extends AtTodoMVCPageWithClearedDataAfterEachTest {
     @Test
     public void testTasksLifeCycle() {
 
-        given("1");
+        given(activeTask("1"));
         toggle("1");
         assertTasks("1");
 
@@ -45,11 +46,11 @@ public class TodoMVCTest extends AtTodoMVCPageWithClearedDataAfterEachTest {
     @Test
     public void testEditAtAll() {
         //given
-        add("1", "2");
+        given(activeTask("1"), completedTask("2"));
 
         edit("2", "2 edited");
         assertTasks("1", "2 edited");
-        assertItemsLeft(2);
+        assertItemsLeft(1);
     }
 
     @Test
@@ -155,25 +156,67 @@ public class TodoMVCTest extends AtTodoMVCPageWithClearedDataAfterEachTest {
    //  executeJavaScript("localStorage.setItem(\"todos-troopjs\", \"[{\\\"completed\\\":false, \\\"taskTexts\\\":\\\"new\\\"},{\\\"completed\\\":false, \\\"taskTexts\\\":\\\"new\\\"}]\")");
    //}
 
-    private String makeGivenCommand(String... tasks) {
+
+
+    private String makeGivenCommand(Task... tasks) {
         String results = "localStorage.setItem(\"todos-troopjs\", \"[";
 
-        for (String task : tasks) {// не понятна конструкция (String task : tasks). Понимаю, что тут надо создать класс. Но как можно упростить для начала?
-            results += "{\\\"completed\\\":false, \\\"taskTexts\\\":\\\"tasks\\\"},"; // тут tasks - не параметр, а значение строки. Вообщем в данном месте самые большие проблемы.
+        for (Task task : tasks) {// не понятна конструкция (String taskText : taskTexts). Понимаю, что тут надо создать класс. Но как можно упростить для начала?
+            results += "{\\\"completed\\\":"+ task.isCompleted +", \\\"title\\\":\\\"" + task.text + "\\\"} , ";
         }
         if (tasks.length > 0) {
             results = results.substring(0, (results.length() - 2));
         }
-        results = results + "]')";
+        results = results + "]  \" )";
 
         System.out.println(results);
         return results;
     }
 
-    public void given(String... tasks) {
+    public void givenActive(String... taskTexts) {
+        given(getTasks(false, taskTexts));
+
+    }
+    public void givenCompleted(String... taskTexts) {
+
+        given(getTasks(true, taskTexts));
+
+    }
+
+    public Task[] getTasks (boolean isCompleted, String... taskTexts){
+        Task[] tasks = new Task[taskTexts.length];
+
+        for (int i = 0; i <taskTexts.length; i++){
+            tasks[i] = new Task(taskTexts[i], isCompleted);
+        }
+        return tasks;
+    }
+
+
+    public void given(Task... tasks) {
         String jsCommand = makeGivenCommand(tasks);
         executeJavaScript(jsCommand);
         refresh();
+    }
+
+
+    public static class Task {
+        String text;
+        boolean isCompleted;
+
+        public static Task activeTask (String text){
+            return new Task(text, false);
+
+        }
+
+        public static Task completedTask (String text) {
+            return new Task(text, true);
+        }
+
+        public Task(String text, boolean isCompleted) {
+            this.text = text;
+            this.isCompleted = isCompleted;
+        }
     }
 
 }
