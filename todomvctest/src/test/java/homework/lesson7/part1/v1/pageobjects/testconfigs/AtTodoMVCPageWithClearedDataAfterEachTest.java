0d71d9b0e2695efebfb5e0066fad2e0051a7bd5e
Oleg <iakovenkoua@gmail.com>
2017-03-01 20:47:57 +0200

    test

--- todomvctest/src/test/java/homework/lesson7/part1/v1/pageobjects/testconfigs/AtTodoMVCPageWithClearedDataAfterEachTest.java
+++ todomvctest/src/test/java/homework/lesson7/part1/v1/pageobjects/testconfigs/AtTodoMVCPageWithClearedDataAfterEachTest.java
@@ -1,15 +1,11 @@
 package homework.lesson7.part1.v1.pageobjects.testconfigs;
-
 import org.junit.After;
 import org.junit.Before;
-
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
 
-/**
- * Created by Homr on 2/26/2017.
- */
-public class AtTodoMVCPageWithClearedDataAfterEachTest extends BaseTest {
+
+public class AtTodoMVCPageWithClearedDataAfterEachTest extends BaseTest{
     @Before
     public void openPage() {
         open("https://todomvc4tasj.herokuapp.com/");
