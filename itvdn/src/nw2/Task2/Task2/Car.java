ecd27031974b5dc58e5ae0b4518d2e39242a0d32
Hailag <iakovenkoua@gmail.com>
2017-03-09 17:52:33 +0200

    1

---
+++ itvdn/src/nw2/Task2/Task2/Car.java
@@ -0,0 +1,22 @@
+package nw2.Task2;
+
+public class Car {
+    private int year;
+    private String color;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ itvdn/src/nw2/Task2/Task2/Main.java
@@ -0,0 +1,9 @@
+package nw2.Task2;
+
+
+public class Main {
+
+    public static void main (String [] args){
+        
+    }
+}
