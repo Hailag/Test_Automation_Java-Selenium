2ed8c7a36a5fe264a6d262ee19a5fff84eb23cee
Hailag <iakovenkoua@gmail.com>
2017-03-20 18:58:23 +0200

    lesson8

---
+++ todomvctest/src/test/java/homework/Lesson8/testsuits/junit/TodoMVCGeneralTest.java
@@ -0,0 +1,36 @@
+package homework.Lesson8.testsuits.junit;
+
+import homework.Lesson8.testsuits.junit.pages.TodoMVCPage;
+import org.junit.Test;
+
+
+public class TodoMVCGeneralTest {
+
+    TodoMVCPage page = new TodoMVCPage();
+
+    @Test
+    public void testTasksLifeCycle() {
+        page.given();
+
+        page.add("1");
+        page.toggle("1");
+        page.assertTasks("1");
+
+        page.filterActive();
+        page.assertNoTasks();
+        page.add("2");
+        page.toggleAll();
+        page.assertNoTasks();
+
+        page.filterCompleted();
+        page.assertTasks("1", "2");
+        page.toggle("2");
+        page.assertTasks("1");
+        page.clearCompleted();
+        page.assertNoTasks();
+
+        page.filterAll();
+        page.assertTasks("2");
+    }
+
+}
---
+++ todomvctest/src/test/java/homework/Lesson8/testsuits/maven/TodoMVCAllFilterTest.java
@@ -0,0 +1,96 @@
+package homework.Lesson8.testsuits.maven;
+
+import homework.Lesson8.testsuits.maven.pages.TodoMVCPage;
+import homework.Lesson8.testsuits.maven.testconfigs.BaseTest;
+import org.junit.Test;
+
+import static homework.Lesson8.testsuits.maven.pages.TodoMVCPage.TaskType.*;
+
+
+
+public class TodoMVCAllFilterTest extends BaseTest {
+    TodoMVCPage page = new TodoMVCPage();
+
+    @Test
+    public void testEdit() {
+        page.givenAtAll(page.aTask(ACTIVE, "1"), page.aTask(COMPLETED, "2"));
+
+        page.edit("2", "2 edited");
+        page.assertTasks("1", "2 edited");
+        page.assertItemsLeft(1);
+    }
+
+    @Test
+    public void testCancelEdit() {
+        page.givenAtAll(ACTIVE, "1");
+
+        page.cancelEdit("1", "edited");
+        page.assertTasks("1");
+        page.assertItemsLeft(1);
+    }
+
+    @Test
+    public void testDelete() {
+        page.givenAtAll(page.aTask(ACTIVE, "1"), page.aTask(COMPLETED, "2"));
+
+        page.delete("1");
+        page.assertTasks("2");
+        page.assertItemsLeft(0);
+    }
+    @Test
+    public void testCompleteAll() {
+        page.givenAtAll(ACTIVE,"1", "2");
+
+        page.toggleAll();
+        page.assertTasks("1", "2");
+        page.assertItemsLeft(0);
+    }
+
+    @Test
+    public void testClearCompleted() {
+        page.givenAtAll(COMPLETED, "1","2");
+
+        page.clearCompleted();
+        page.assertNoTasks();
+
+    }
+
+    @Test
+    public void testReopen() {
+        page.givenAtAll(COMPLETED,"1");
+
+        page.toggle("1");
+        page.assertTasks("1");
+        page.assertItemsLeft(1);
+    }
+
+
+    @Test
+    public void testCancelEditByEsc() {
+        page.givenAtAll(page.aTask(ACTIVE, "1"), page.aTask(COMPLETED, "2"));
+
+        page.cancelEdit("2", "2 edited");
+        page.assertTasks("1", "2");
+        page.assertItemsLeft(1);
+    }
+
+    @Test
+    public void testEditByClickOutOfTask() {
+        page.givenAtAll(ACTIVE, "1", "2");
+
+        page.editByClickOutOfTask("2", "2 edited");
+        page.assertTasks("1", "2 edited");
+        page.assertItemsLeft(2);
+    }
+
+    @Test
+    public void testSwitchFromAllToCompleted() {
+        page.givenAtAll(page.aTask(ACTIVE, "1"), page.aTask(COMPLETED, "2"));
+
+        page.filterCompleted();
+        page.assertTasks( "2");
+        page.assertItemsLeft(1);
+    }
+
+
+}
