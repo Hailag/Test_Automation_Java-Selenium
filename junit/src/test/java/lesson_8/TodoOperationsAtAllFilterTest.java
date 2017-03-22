c09a42058168346eac2ff7dce0497d1228006b51
Hailag <iakovenkoua@gmail.com>
2017-03-22 19:03:23 +0200

    new tests class

---
+++ junit/src/test/java/lesson_8/TodoOperationsAtAllFilterTest.java
@@ -0,0 +1,7 @@
+package lesson_8;
+
+/**
+ * Created by o.iakovenko on 22.03.2017.
+ */
+public class TodoOperationsAtAllFilterTest {
+}
---
+++ junit/src/test/java/lesson_8/testconfigs/AtTodoMVCPageWithClearedDataAfterEachTest.java
@@ -0,0 +1,7 @@
+package lesson_8.testconfigs;
+
+/**
+ * Created by o.iakovenko on 22.03.2017.
+ */
+public class AtTodoMVCPageWithClearedDataAfterEachTest {
+}
