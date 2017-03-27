98c7b4f5b8ec072bf3631dac2b05713100f72a9b
Hailag <iakovenkoua@gmail.com>
2017-03-27 18:36:44 +0300

    проблемы с реализацией ожидаемым результатом

--- googletest/src/test/java/lesson/nine/google/pages/GoogleSearch.java
+++ googletest/src/test/java/lesson/nine/google/pages/GoogleSearch.java
@@ -1,4 +1,24 @@
 package lesson.nine.google.pages;
 
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class GoogleSearch {
+
+    public static void googleMainPage() {
+        open("http://google.com/ncr");
+    }
+
+    public static void search(String searchText){
+        $(By.name("q")).setValue(searchText).submit();
+    }
+
+    public static void assertResult(int count){ // как реализовать ожидаемый результат? Где почитать?
+
+    }
+
+
 }
