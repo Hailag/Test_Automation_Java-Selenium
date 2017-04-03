07ace5144b09c48e47805a191738ffcfcc5a2248
Hailag <iakovenkoua@gmail.com>
2017-04-03 18:01:26 +0300

    new

--- gmailtest/src/test/java/lesson/ten/GmailTest.java
+++ gmailtest/src/test/java/lesson/ten/GmailTest.java
@@ -1,5 +1,18 @@
 package lesson.ten;
 
+import org.junit.Test;
+import static lesson.ten.config.TestData.openPage;
+
 public class GmailTest {
+
+    @Test
+    public void testloginGmail() {
+
+        openPage();
+
+
+    }
+
+
 
 }
